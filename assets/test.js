abcd0c5ff0789aa81060f12a4b0280627094e5f0
shandraspertiwi <shandra.2206@gmail.com>
2022-06-25 22:50:24 +0700

    test js

--- assets/test.js
+++ assets/test.js
@@ -1,23 +1,21 @@
 console.log("Congrats!");
 const firstName = prompt("Siapa nama depanmu?");
-const lastName = prompt("Siapa nama belakangmu?");
 const language = prompt("Bisa berbahasa apa?");
  
 const user = {
    name: {
        first: firstName,
-       last: lastName,
    },
    language: language
 };
  
 if (user.language === "English") {
-   alert("Nice to meet you " + user.name.first + " " + user.name.last + "!");
+   alert("Nice to meet you " + user.name.first + " " + "!");
 } else if (user.language === "French") {
-   alert("Ravi de vous rencontrer " + user.name.first + " " + user.name.last + "!");
+   alert("Ravi de vous rencontrer " + user.name.first + " " +  "!");
 } else if (user.language === "Japanese") {
-   alert("Hajimemashite, " + user.name.first + " " + user.name.last + "!");
+   alert("Hajimemashite, " + user.name.first + " " + "!");
 } else {
-   alert("Senang bertemu dengan Anda " + user.name.first + " " + user.name.last + "!");
+   alert("Senang bertemu dengan Anda " + user.name.first + " " +  "!");
 }
 
