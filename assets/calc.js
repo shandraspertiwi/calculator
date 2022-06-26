7f77c61dcdb219203039f1d799435dae2bd5f51c
shandraspertiwi <shandra.2206@gmail.com>
2022-06-26 21:46:05 +0700

    add js to calc

--- assets/calc.js
+++ assets/calc.js
@@ -1,1 +1,42 @@
-console.log("Congrats!");
+const calculator = {
+    displayNumber: '0',
+    operator: null,
+    firstNumber: null,
+    waitingForSecondNumber: false,
+  };
+   
+  function updateDisplay() {
+    document.querySelector('#displayNumber').innerText = calculator.displayNumber;
+  }
+   
+  function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.waitingForSecondNumber = false;
+  }
+   
+  function inputDigit(digit) {
+    if (calculator.displayNumber === '0') {
+      calculator.displayNumber = digit;
+    } else {
+      calculator.displayNumber += digit;
+    }
+  }
+   
+  const buttons = document.querySelectorAll('.button');
+  for (const button of buttons) {
+    button.addEventListener('click', function(event) {
+      // mendapatkan objek elemen yang diklik
+      const target = event.target;
+   
+      if (target.classList.contains('clear')) {
+        clearCalculator();
+        updateDisplay();
+        return;
+      }
+   
+      inputDigit(target.innerText);
+      updateDisplay();
+    });
+  }
